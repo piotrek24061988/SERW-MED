38e8ad01a03144606eaa2fee6630119b327dde18
Gorecki Piotr <piotr.gorecki@meelogic.com>
2020-04-04 18:29:42 +0200

    initial img added to news

--- serw_med/news/forms.py
+++ serw_med/news/forms.py
@@ -1,12 +1,14 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
+from django.forms.widgets import ClearableFileInput
 from .models import News
 
 
 class NewsCreateForm(forms.ModelForm):
     title = forms.CharField(label=_('Tytuł'))
     content = forms.CharField(widget=forms.Textarea, label=_('Treść'))
+    image = forms.ImageField(label=_("Zdjęcie"), widget=ClearableFileInput, required=False)
 
     class Meta:
         model = News
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'image']
--- serw_med/news/models.py
+++ serw_med/news/models.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 class News(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    image = models.ImageField(upload_to='news_pics', null=True)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
