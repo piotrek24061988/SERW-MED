f368bea3cbc863e0d21c842b527d6fd74a608482
Gorecki Piotr <piotr.gorecki@meelogic.com>
2020-04-09 15:13:44 +0200

    Overwriting old files instead of adding hashes to the name

--- serw_med/news/models.py
+++ serw_med/news/models.py
@@ -2,12 +2,23 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+import os
+
+
+class OverwriteStorage(FileSystemStorage):
+    def get_available_name(self, name, max_length=None):
+        # If the filename already exists, remove it as if it was a true file system
+        if self.exists(name):
+            os.remove(os.path.join(settings.MEDIA_ROOT, name))
+        return name
 
 
 class News(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField(upload_to='news_pics', null=True)
+    image = models.ImageField(upload_to='news_pics', storage=OverwriteStorage(), null=True)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
--- serw_med/users/models.py
+++ serw_med/users/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+import os
+
+
+class OverwriteStorage(FileSystemStorage):
+    def get_available_name(self, name, max_length=None):
+        # If the filename already exists, remove it as if it was a true file system
+        if self.exists(name):
+            os.remove(os.path.join(settings.MEDIA_ROOT, name))
+        return name
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='profile_pics/DefaultMan.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='profile_pics/DefaultMan.jpg', upload_to='profile_pics', storage=OverwriteStorage())
 
     def __str__(self):
         return f'{self.user.username}Profile'
