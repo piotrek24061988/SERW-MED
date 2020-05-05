ce3c22f60b82ecc8e8868786e347411cbc8b6cc7
Gorecki Piotr <piotr.gorecki@meelogic.com>
2020-05-05 13:50:23 +0200

    date leading 0 improvement

--- serw_med/news/static/timeAndDate.js
+++ serw_med/news/static/timeAndDate.js
@@ -2,7 +2,9 @@ function currentTimeAndDate()
 {
     var today = new Date();
     var day = today.getDate();
+    if(day < 10) day = "0" + day;
     var month = today.getMonth()+1;
+    if(month < 10) month = "0" + month;
     var year = today.getFullYear();
     var hours = today.getHours();
     if(hours < 10) hours = "0" + hours;
