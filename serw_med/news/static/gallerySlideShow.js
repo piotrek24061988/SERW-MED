e0ff61b6f833d459afbfc994f8091dae2fe4b175
Gorecki Piotr <piotr.gorecki@meelogic.com>
2020-05-06 12:04:27 +0200

    better slide transition

--- serw_med/news/static/gallerySlideShow.js
+++ serw_med/news/static/gallerySlideShow.js
@@ -1,12 +1,18 @@
 var maxNumber = 4;
 var number = 0;
 
+function hideSlide() {
+    $('#gallerySlider').fadeOut(1000);
+}
+
 function changeSlide() {
     number++;
     if(number > maxNumber) number = 1;
 
-    var file = "<img src=\"/media/gallery/" + number + ".jpg\">";
+    var file = "<img src=\"/media/gallery/" + number + ".jpg\"/>";
     document.getElementById("gallerySlider").innerHTML = file;
+    $('#gallerySlider').fadeIn(1000);
 
+    setTimeout("hideSlide()", 4000);
     setTimeout("changeSlide()", 5000);
 }
