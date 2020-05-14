084a7dec4b68df4054e666847aaad3b7fe4bb641
Gorecki Piotr <piotr.gorecki@meelogic.com>
2020-05-14 13:34:28 +0200

    static issue fixed

--- serw_med/users/tests/tests_views.py
+++ serw_med/users/tests/tests_views.py
@@ -8,25 +8,25 @@ class UsersViewsTestCases(unittest.TestCase):
     request = stubs.RequestStub
     response_status = 200
 
-    #def test_register(self):
-    #    # Setup
-    #    UsersViewsTestCases.request.method = 'POST'
-    #    response_content = b'Rejestracja'
-    #    # Run
-    #    response = views.SerwMedUsers.register(UsersViewsTestCases.request)
-    #    # Check
-    #    self.assertEqual(response.status_code, UsersViewsTestCases.response_status)
-    #    self.assertIn(response_content, response.content)
+    def test_register(self):
+        # Setup
+        UsersViewsTestCases.request.method = 'POST'
+        response_content = b'Rejestracja'
+        # Run
+        response = views.SerwMedUsers.register(UsersViewsTestCases.request)
+        # Check
+        self.assertEqual(response.status_code, UsersViewsTestCases.response_status)
+        self.assertIn(response_content, response.content)
 
-    #def test_profile_get(self):
-    #    # Setup
-    #    UsersViewsTestCases.request.method = 'GET'
-    #    response_content = b'Informacje o profilu'
-    #    # Run
-    #    response = views.SerwMedUsers.profile(UsersViewsTestCases.request)
-    #    # Check
-    #    self.assertEqual(response.status_code, UsersViewsTestCases.response_status)
-    #    self.assertIn(response_content, response.content)
+    def test_profile_get(self):
+        # Setup
+        UsersViewsTestCases.request.method = 'GET'
+        response_content = b'Informacje o profilu'
+        # Run
+        response = views.SerwMedUsers.profile(UsersViewsTestCases.request)
+        # Check
+        self.assertEqual(response.status_code, UsersViewsTestCases.response_status)
+        self.assertIn(response_content, response.content)
 
     def test_profile_post(self):
         # Setup
