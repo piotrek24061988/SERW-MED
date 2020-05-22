ecfdbd633b6075ef1f043a17ff5a26235699ce6e
Gorecki Piotr <piotr.gorecki@meelogic.com>
2020-05-22 17:40:36 +0200

    store views urls registered

--- serw_med/serw_med/urls.py
+++ serw_med/serw_med/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as users_view
+from store import views as store_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +27,9 @@ urlpatterns = [
     path('login/', users_view.CustomLoginView.as_view(template_name='login.html'), name='serw-med-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='serw-med-logout'),
     path('profile/', users_view.SerwMedUsers.profile, name='serw-med-profile'),
+    path('store/', store_views.SerwMedStore.store, name='serw-med-store'),
+    path('cart/', store_views.SerwMedStore.cart, name='serw-med-cart'),
+    path('checkout/', store_views.SerwMedStore.checkout, name='serw-med-checkout'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'),
          name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
