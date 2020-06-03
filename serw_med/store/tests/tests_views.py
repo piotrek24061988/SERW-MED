4c5bc245e8dae168d37e167fc059d629ef253d42
Gorecki Piotr <piotr.gorecki@meelogic.com>
2020-06-03 18:08:28 +0200

    fixing tests

--- serw_med/store/tests/tests_views.py
+++ serw_med/store/tests/tests_views.py
@@ -13,32 +13,32 @@ class StoreViewsTestCases(unittest.TestCase):
     request.COOKIES['cart'] = '{"get_cart_total": 0, "get_cart_items": 0, "shipping": "False"}'
     response_status = 200
 
-    #def test_store(self):
-    #    # Setup
-    #    StoreViewsTestCases.request.method = 'POST'
-    #    response_content = b'sklep'
-    #    # Run
-    #    response = views.SerwMedStore.store(StoreViewsTestCases.request)
-    #    # Check
-    #    self.assertEqual(response.status_code, StoreViewsTestCases.response_status)
-    #    self.assertIn(response_content, response.content)
+    def test_store(self):
+        # Setup
+        StoreViewsTestCases.request.method = 'POST'
+        response_content = b'sklep'
+        # Run
+        response = views.SerwMedStore.store(StoreViewsTestCases.request)
+        # Check
+        self.assertEqual(response.status_code, StoreViewsTestCases.response_status)
+        self.assertIn(response_content, response.content)
 
-    #def test_cart(self):
-    #    # Setup
-    #    StoreViewsTestCases.request.method = 'POST'
-    #    response_content = b'lista'
-    #    # Run
-    #    response = views.SerwMedStore.cart(StoreViewsTestCases.request)
-    #    # Check
-    #    self.assertEqual(response.status_code, StoreViewsTestCases.response_status)
-    #    self.assertIn(response_content, response.content)
+    def test_cart(self):
+        # Setup
+        StoreViewsTestCases.request.method = 'POST'
+        response_content = b'lista'
+        # Run
+        response = views.SerwMedStore.cart(StoreViewsTestCases.request)
+        # Check
+        self.assertEqual(response.status_code, StoreViewsTestCases.response_status)
+        self.assertIn(response_content, response.content)
 
-    #def test_checkout(self):
-    #    # Setup
-    #    StoreViewsTestCases.request.method = 'POST'
-    #    response_content = b'potwierdzenie'
-    #    # Run
-    #    response = views.SerwMedStore.checkout(StoreViewsTestCases.request)
-    #    # Check
-    #    self.assertEqual(response.status_code, StoreViewsTestCases.response_status)
-    #    self.assertIn(response_content, response.content)
+    def test_checkout(self):
+        # Setup
+        StoreViewsTestCases.request.method = 'POST'
+        response_content = b'potwierdzenie'
+        # Run
+        response = views.SerwMedStore.checkout(StoreViewsTestCases.request)
+        # Check
+        self.assertEqual(response.status_code, StoreViewsTestCases.response_status)
+        self.assertIn(response_content, response.content)
