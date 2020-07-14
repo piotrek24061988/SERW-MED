97740e150f1c28f413cab983c3e9cd4a369add02
Gorecki Piotr <piotr.gorecki@meelogic.com>
2020-07-14 16:20:41 +0200

    views tests added

--- serw_med/store/tests/tests_views.py
+++ serw_med/store/tests/tests_views.py
@@ -1,8 +1,9 @@
 from django.test.client import RequestFactory
 from django.contrib.auth.models import AnonymousUser
 import unittest
-import io
 from .. import views
+from django.test import TestCase, Client
+from django.urls import reverse
 
 
 class StoreViewsTestCases(unittest.TestCase):
@@ -40,3 +41,53 @@ class StoreViewsTestCases(unittest.TestCase):
         # Check
         self.assertEqual(response.status_code, StoreViewsTestCases.response_status)
         self.assertIn(response_content, response.content)
+
+
+class StoreViewsDjangoTestCases(TestCase):
+    # Common setup
+    def setUp(self):
+        self.client = Client()
+
+    def test_store_GET(self):
+        # Setup
+        url = reverse('serw-med-store')
+        # Run
+        response = self.client.get(url)
+        # Check
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'store.html')
+
+    def test_cart_GET(self):
+        # Setup
+        url = reverse('serw-med-cart')
+        # Run
+        response = self.client.get(url)
+        # Check
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cart.html')
+
+    def test_checkout_GET(self):
+        # Setup
+        url = reverse('serw-med-checkout')
+        # Run
+        response = self.client.get(url)
+        # Check
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'checkout.html')
+
+    def test_fake_product_GET(self):
+        # Setup
+        url = reverse('serw-med-product', args=['0'])
+        # Run
+        response = self.client.get(url)
+        # Check
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateNotUsed(response, 'product_single.html')
+
+    def test_update_item_GET(self):
+        # Setup
+        url = reverse('serw-med-update-item')
+        # Run
+        response = self.client.get(url)
+        # Check
+        self.assertEqual(response.status_code, 200)
